ad17c448b8326d0e2972811ae158966f92f09332
ahmed-fs-dev <ahmed.bahloul.dev@gmail.com>
2025-11-29 10:51:54 +0100

    Initial project setup

---
+++ shelf-track-resource-server/src/main/java/org/example/shelftrack/ShelfTrackApplication.java
@@ -0,0 +1,13 @@
+package org.example.shelftrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfTrackApplication.class, args);
+    }
+
+}
---
+++ shelf-track-resource-server/src/test/java/org/example/shelftrack/ShelfTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shelftrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
